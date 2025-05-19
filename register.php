943749fd2d015351abc99313a37aa0ecacec608e
niceinfo23 <delajntii@gmail.com>
2025-05-19 19:46:02 +0700

    Create register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+include 'koneksi.php';
+
+$username = $_POST['username'];
+$email    = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+// Validasi
+if (!empty($username) && !empty($email) && !empty($_POST['password'])) {
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    if ($conn->query($sql)) {
+        echo "Registrasi berhasil!";
+    } else {
+        echo "Email sudah terdaftar.";
+    }
+} else {
+    echo "Semua field harus diisi!";
+}
+?>
